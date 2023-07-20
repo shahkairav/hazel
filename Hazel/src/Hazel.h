9bad0515af2abde400de5ce3f62b46b18fb53e56
Kairav Shah <kairavshah1702@gmail.com>
2023-07-20 23:59:22 +0100

    repenting for not learning VS properly earlier

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// FOR USE BY HAZEL APPLICATIONS
+#include "Hazel/Application.h"
+
+// --Entry Point----------------------
+#include "Hazel/EntryPoint.h"
+// -----------------------------------
